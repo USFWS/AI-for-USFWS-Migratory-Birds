0755c254ba09d80fc08b103724cde90517703494
bpickens99 <bradley_pickens@fws.gov>
2024-08-02 10:04:28 -0400

    Update Converts LabelME to COCO.py

--- Annotation conversions/Converts LabelME to COCO.py
+++ Annotation conversions/Converts LabelME to COCO.py
@@ -2,6 +2,7 @@ import labelme2coco
 
 # Inputs: annot_dir = dir that contains labelme annotation files;
 #  export_dir = dir to save newly formatted annotations
+
 annot_dir = "C:/Users/bpickens/Desktop/testing/"
 export_dir = "C:/Users/bpickens/Desktop/testing/new"
 
