b6ea4cf701bd26cb5e503478546d2eb002ee68e4
bpickens99 <bradley_pickens@fws.gov>
2024-12-06 15:56:46 -0500

    Update Yolov8 inference w tiles.py

--- YOLO detection models/Yolov8 inference w tiles.py
+++ YOLO detection models/Yolov8 inference w tiles.py
@@ -46,10 +46,8 @@ for root, dirs, files in os.walk(root_dir):
             writer = csv.writer(file)
             for result1 in object_prediction_list:
                 writer.writerow([source, result1.bbox, result1.category, result1.score])
-    
 csv_data = pd.read_csv(new_csv)
-
-filename = csv_data['unique_image_jpg']
+csv_data['unique_image_jpg'] = csv_data['unique_image_jpg'].apply(os.path.basename)
 bbox = csv_data['bbox']
 csv_data['score'] = csv_data['score'].str.replace(r"PredictionScore: <value: ", '', regex=True)
 csv_data['score'] = csv_data['score'].str.replace(r">", '', regex=True)
@@ -62,7 +60,18 @@ csv_data[['xmin', 'ymin', 'xmax', 'ymax', 'w', 'h']] = csv_data['bbox'].str.spli
 csv_data['h'] = csv_data['h'].str.replace(r"h: ", '', regex= True)
 csv_data['w'] = csv_data['w'].str.replace(r"w: ", '', regex= True)
 csv_data['xmin'] = csv_data['xmin'].str.replace(r"(", '', regex= False)
+csv_data['temp_name'] = csv_data['unique_image_jpg'].str.replace(r".jpg", '', regex= True)
+csv_data['xmin'] = csv_data['xmin'].astype(float).round().astype(int).astype(str)
+csv_data['ymin'] = csv_data['ymin'].astype(float).round().astype(int).astype(str)
+csv_data['w'] = csv_data['w'].astype(float).round().astype(int).astype(str)
+csv_data['h'] = csv_data['h'].astype(float).round().astype(int).astype(str)
+
+csv_data['unique_BB'] = csv_data['temp_name'] + "_" + csv_data['xmin'] + "_" + csv_data['ymin'] + "_" + csv_data['w']+ "_" + csv_data['h']
 del csv_data['bbox']
-csv_data.to_csv(new_csv)
+del csv_data['xmax']
+del csv_data['ymax']
+del csv_data['temp_name']
+csv_data.to_csv(new_csv)   
+
 
 
