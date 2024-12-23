146b325e6e7d0885eb1e87b418399f55a9da908b
bpickens99 <bradley_pickens@fws.gov>
2024-12-23 11:18:51 -0500

    Update Rename file extension in folder.py
    updated renaming conventions

--- Imagery mgt and preprocessing/Rename file extension in folder.py
+++ Imagery mgt and preprocessing/Rename file extension in folder.py
@@ -1,9 +1,14 @@
-import glob, os
+import glob,os
 
 # Input dir where file extensions need to be changed
-os.chdir('F:/AMAPPS_CLASSIFY/silly')
+os.chdir('E:/Flights/20241219_150200_rawonly/Overviews/0')
+source_dir = 'E:/Flights/20241219_150200_rawonly/Overviews/0/'
+export_dir = 'E:/Flights/20241219_150200_rawonly/Overviews/new2/'
 
 for filename in glob.glob('*.JPG'):
     print(filename)
     pre, ext = os.path.splitext(filename)
-    os.rename(filename, pre + '.jpg') # enter the new filename extension
+    rename1 = pre + '.jpg'
+    print ("Rename: ", rename1)
+    os.rename(os.path.join(source_dir, filename),
+              os.path.join(export_dir, rename1)) # enter the new filename extension
